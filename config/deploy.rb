5c9bba1b520abc98c33b8940643c13cb4110740b
michal-kazmierczak <michal.kazmierczak@netguru.co>
2016-04-10 01:12:25 +0200

    app deployment with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+lock '3.4.0'
+
+set :application, "swearbot"
+set :repo_url, "git@github.com:mabzd/SwearBot.git"
+
+set :linked_files, %w(bin/config.json bin/config.json log.txt)
+set :keep_releases, 5
+set :deploy_to, "/var/go/swearbot"
+
+namespace :app do
+  task :compile do
+    on roles(:app) do
+      execute "cd #{current_path} && mkdir -p bin"
+      execute "cd #{current_path} && cp swears.txt bin/swears.txt"
+      execute "cd #{current_path} && GOPATH=/home/michal/go go build -o ./bin/swbot.exe main.go"
+    end
+  end
+
+  %i(start stop).each do |command|
+    task command do
+      on roles(:app) do
+        execute "/etc/init.d/swbot #{command}"
+      end
+    end
+  end
+end
+
+after "deploy", "app:compile"
+after "deploy", "app:start"
