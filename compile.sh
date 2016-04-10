cd003cc0746875c8c7e83a462de36367663aae14
mabzd <marcin.bzdziuch@gmail.com>
2016-04-10 18:56:05 +0200

    Fixed build scripts

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p bin
-cp -n ./config.json ./bin/config.json
-cp -n ./swears.txt ./bin/swears.txt
+cp -u ./config-rename.json ./bin/config.json
+cp -u ./swears.txt ./bin/swears.txt
 go build -o ./bin/swbot.exe main.go
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./bin/swbot.exe
+cd ./bin
+./swbot.exe
